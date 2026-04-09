b3e7aef155b45c6337e0010a88b3d2932a5d1136
Adrián Aguilar Porcel <adriiagui81@gmail.com>
2026-04-09 18:15:07 +0200

    Milestone 2: Modify productRoutes error

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,4 +1,4 @@
-const express = requiere("express");
+const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productControllers");
 
