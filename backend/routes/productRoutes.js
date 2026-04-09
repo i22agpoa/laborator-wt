8278edb2096ad4794b4a5d75293ef667128cb8c1
Adrián Aguilar Porcel <adriiagui81@gmail.com>
2026-04-09 18:07:54 +0200

    Milestone 2: Create productRoutes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,20 @@
+const express = requiere("express");
+const router = express.Router();
+const productController = require("../controllers/productControllers");
+
+// Get all products
+router.get("/", productController.getAllProducts);
+
+// Get one product by ID
+router.get("/", productController.getProductById);
+
+// Create product
+router.get("/", productController.createProduct);
+
+// Update product
+router.get("/", productController.updateProduct);
+
+// Delete product
+router.get("/", productController.deleteProduct);
+
+module.exports = router;
