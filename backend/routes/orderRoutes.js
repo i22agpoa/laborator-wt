a823813eb78081b4f9a38d2183bb1cecd6baf8b5
Adrián Aguilar Porcel <adriiagui81@gmail.com>
2026-04-11 17:10:49 +0200

    Milestone 2: Create orderRoutes

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const orderController = require("../controllers/orderController");
+
+// Get all orders
+router.get("/", orderController.getAllOrders);
+
+// Get one order by ID
+router.get("/:id", orderController.getOrderById);
+
+// Create order
+router.post("/", orderController.createOrder);
+
+// Update order
+router.put("/:id", orderController.updateOrder);
+
+// Delete order
+router.delete("/:id", orderController.deleteOrder);
+
+module.exports = router;
