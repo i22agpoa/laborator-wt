bd193b3e17932d89e696a72b0d65c6b5eb9b5111
Adrián Aguilar Porcel <adriiagui81@gmail.com>
2026-04-12 17:12:32 +0200

    Milestone 2: Create roleMiddleware and modify productRoutes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
+const { authenticate } = require("../middleware/authMiddleware");
+const { authorize } = require("../middleware/roleMiddleware");
+
+// PUBLIC
 
 // Get all products
 router.get("/", productController.getAllProducts);
@@ -8,13 +12,27 @@ router.get("/", productController.getAllProducts);
 // Get one product by ID
 router.get("/:id", productController.getProductById);
 
+// ADMIN ONLY
+
 // Create product
-router.post("/", productController.createProduct);
+router.post("/", 
+    authenticate,
+    authorize("admin"), 
+    productController.createProduct
+);
 
 // Update product
-router.put("/:id", productController.updateProduct);
+router.put("/:id",
+    authenticate,
+    authorize("admin"),
+    productController.updateProduct
+);
 
 // Delete product
-router.delete("/:id", productController.deleteProduct);
+router.delete("/:id",
+    authenticate,
+    authorize("admin"),
+    productController.deleteProduct
+);
 
 module.exports = router;
