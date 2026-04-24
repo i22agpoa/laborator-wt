52b2e335ee66126449dedc04ac51d6959c34fe91
Adrián Aguilar Porcel <adriiagui81@gmail.com>
2026-04-24 12:30:09 +0200

    Milestone 3: Modify authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -9,11 +9,20 @@ import axios from "../utils/axiosConfig";
  * 4. Returnarea datelor primite de la server pentru fiecare operațiune.
  */
 
-const register = async (userData) => {};
+const register = async (userData) => {
+	const response = await axios.post("/auth/register", userData);
+	return response.data;
+};
 
-const login = async (credentials) => {};
+const login = async (credentials) => {
+	const response = await axios.post("/auth/login", credentials);
+	return response.data;
+};
 
-const getProfile = async () => {};
+const getProfile = async () => {
+	const response = await axios.get("/auth/profile");
+	return response.data;
+};
 
 const authService = {
 	register,
